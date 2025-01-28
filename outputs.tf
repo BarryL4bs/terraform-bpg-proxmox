f6ee763549a33fbb67fc72502d5601d3c477452f
Roberto Santos <74910247+rbsantosbr@users.noreply.github.com>
2025-01-28 09:35:04 -0300

    Update outputs.tf
    Change output name to "api_token".

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "id" {
   value = proxmox_virtual_environment_user_token.pve_user_token.id
 }
 
-output "api-token" {
+output "api_token" {
   value     = proxmox_virtual_environment_user_token.pve_user_token.value
   sensitive = true
 }
